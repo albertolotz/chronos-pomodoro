8f7fa0e28a7fcfe21bb60cad0a636704bbaf1156
Alberto Lotz <alberto10lotz@gmail.com>
2025-06-22 18:49:42 -0300

    1.0.15: Implement task creation in MainForm and log state changes in TaskContextProvider - começar aula 54

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,16 +4,64 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
+  //const [taskName, setTaskName] = useState('');
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Não foi enviada uma tarefa...');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(), //um codigo, poderia ser outro mdodelo
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaning = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // conferir
+        secondsRemaning, // conferir
+        formattedSecondsRemaning: '00:00', // conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           id='meuInput'
           type='text'
           labelText='Task'
           placeholder='Digite Algo'
+          // value={taskName}
+          // onChange={e => {
+          //   setTaskName(e.target.value);
+          // }}
+          ref={taskNameInput}
         />
       </div>
 
